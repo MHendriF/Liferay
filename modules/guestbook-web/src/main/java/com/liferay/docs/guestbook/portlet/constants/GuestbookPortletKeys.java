569a008daa68a59517b47d2b5e143cc2152862dc
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2019-01-28 11:57:09 +0700

    Creating a Working Prototype - part2

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author hendr
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "guestbook";
+
+}
